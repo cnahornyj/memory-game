97fc7a7f7e60150cb5a2f98cf85ca6b350e3af31
stéphane <gervaisstephane@orange.fr>
2019-09-26 09:43:43 +0200

    modif json

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 const img = [ 
     {
-        numberImg : 0;
-        pictures : src="";
+        numberImg : 0,
+        pictures : src=""
     },
     {
-        numberImg : 1;
-        pictures : src="";
+        numberImg : 1,
+        pictures : src=""
     },
     {
-        numberImg : 2;
-        pictures : src="";
+        numberImg : 2,
+        pictures : src=""
     },
     {
-        numberImg : 3;
-        pictures : src="";
+        numberImg : 3,
+        pictures : src=""
     },
     {
-        numberImg : 4;
-        pictures : src="";
+        numberImg : 4,
+        pictures : src=""
     },
     {
-        numberImg : 5;
-        pictures : src="";
+        numberImg : 5,
+        pictures : src=""
     },
     {
-        numberImg : 6;
-        pictures : src="";
+        numberImg : 6,
+        pictures : src=""
     },
     {
-        numberImg : 7;
-        pictures: src="";
+        numberImg : 7,
+        pictures: src=""
     },
 ]
 
